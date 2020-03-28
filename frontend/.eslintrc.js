f80dc6712600506a5ae4e2b7bfd1c8ff2d86e6a6
AndersonASouza <andersonandredesouza01@gmail.com>
2020-03-28 02:28:05 -0300

    Front-end do app

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["prettier", "react", "jsx-a11y", "import"],
+  rules: {
+    "prettier/prettier": ["error"],
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".js", ".jsx"]
+      }
+    ],
+
+    "global-require": "off",
+    "import/prefer-default-export": "off",
+    "no-unused-expressions": ["error", { allowTaggedTemplates: true }]
+  }
+};
